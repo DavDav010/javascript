ff7bc141f118a65f00755efa98046780ed62932f
David <123590426+DavDav010@users.noreply.github.com>
2024-04-14 19:46:51 +0400

    10a,b,c,d,e,f

---
+++ scripts/10-e,f.js
@@ -0,0 +1,20 @@
+        const button = document.querySelector('.js-button');
+        if(!button.classList.contains('is-turned-on')){
+            button.classList.add('is-turned-on');
+        } else{
+            button.classList.remove('is-turned-on');
+        }
+
+        const button = document.querySelector('.js-button-2');
+        if(!button.classList.contains('is-turned-on')){
+            button.classList.add('is-turned-on');
+        } else{
+            button.classList.remove('is-turned-on');
+        }
+
+        const button = document.querySelector('.js-button-3');
+        if(!button.classList.contains('is-turned-on')){
+            button.classList.add('is-turned-on');
+        } else{
+            button.classList.remove('is-turned-on');
+        }
